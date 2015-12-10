7663a9a871f9d0f6dfe34d5f3876493786e07f19
nkvaltine <nkvaltine@ecotope.com>
2015-12-09 17:11:02 -0800

    add updateTemperatures function, including proper outletT calculation

--- dev/HPWH.cc
+++ dev/HPWH.cc
@@ -11,44 +11,47 @@ int HPWH::HPWHinit_presets(int presetNum)
 	if(presetNum == 1){
 		
 		numNodes = 12;
-		tankVolume = 50;
+		tankVolume_L = 189.271; //50 gallons
 		
 		doTempDepression = false;
 		
 		numElements = 1;
-		for(int i = 0; i < numElements; i++){
-			setOfElements[i] = Element(this);
-		}
-		
-		setOfElements[0].isEngaged = false;
-		
+		setOfElements = new Element[numElements];
+
 		//set up a resistive element at the bottom, 4500 kW
-		setOfElements[0].setCondensity(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+		Element resistiveElement(this);
+		
+		resistiveElement.isEngaged = false;
+		
+		resistiveElement.setCondensity(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
 				
-		setOfElements[0].T1 = 50;
-		setOfElements[0].T2 = 67;
+		resistiveElement.T1 = 50;
+		resistiveElement.T2 = 67;
 		
-		setOfElements[0].inputPower_T1_constant = 4500;
-		setOfElements[0].inputPower_T1_linear = 0;
-		setOfElements[0].inputPower_T1_quadratic = 0;
+		resistiveElement.inputPower_T1_constant = 4500;
+		resistiveElement.inputPower_T1_linear = 0;
+		resistiveElement.inputPower_T1_quadratic = 0;
 		
-		setOfElements[0].inputPower_T2_constant = 4500;
-		setOfElements[0].inputPower_T2_linear = 0;
-		setOfElements[0].inputPower_T2_quadratic = 0;
+		resistiveElement.inputPower_T2_constant = 4500;
+		resistiveElement.inputPower_T2_linear = 0;
+		resistiveElement.inputPower_T2_quadratic = 0;
 		
-		setOfElements[0].COP_T1_constant = 1;
-		setOfElements[0].COP_T1_linear = 0;
-		setOfElements[0].COP_T1_quadratic = 0;
+		resistiveElement.COP_T1_constant = 1;
+		resistiveElement.COP_T1_linear = 0;
+		resistiveElement.COP_T1_quadratic = 0;
 		
-		setOfElements[0].COP_T2_constant = 1;
-		setOfElements[0].COP_T2_linear = 0;
-		setOfElements[0].COP_T2_quadratic = 0;
+		resistiveElement.COP_T2_constant = 1;
+		resistiveElement.COP_T2_linear = 0;
+		resistiveElement.COP_T2_quadratic = 0;
 		
-		setOfElements[0].lowTlockout = -100;	//no lockout
-		setOfElements[0].hysteresis = 0;	//no hysteresis
+		resistiveElement.lowTlockout = -100;	//no lockout
+		resistiveElement.hysteresis = 0;	//no hysteresis
 
-		setOfElements[0].depressesTemperature = false;  //no temp depression
+		resistiveElement.depressesTemperature = false;  //no temp depression
 
+		
+		//assign elements into array in order of priority
+		setOfElements[0] = resistiveElement;
 		
 	}
 	
@@ -61,10 +64,51 @@ int HPWH::HPWHinit_presets(int presetNum)
 
 
 
-int HPWH::runOneStep(double inletT, double drawVolume, 
-					double ambientT, double externalT,
-					double DRstatus)
+int HPWH::runOneStep(double inletT_C, double drawVolume_L, 
+					double ambientT_C, double externalT_C,
+					double DRstatus, double minutesPerStep)
 {
+
+
+
+
+//reset the output variables
+outletTemp_C = 0;
+energyRemovedFromEnvironment_kWh = 0;
+standbyLosses_kWh = 0;
+
+for(int i = 0; i < numElements; i++){
+	setOfElements[i].runtime = 0;
+	setOfElements[i].energyInput = 0;
+	setOfElements[i].energyOutput = 0;
+}
+
+
+
+//process draws and standby losses
+updateTankTemps(drawVolume_L, inletT_C, ambientT_C, minutesPerStep);
+
+
+
+//do element choice
+
+
+
+//change the things according to DR schedule
+
+
+
+//do heating logic
+
+
+
+
+//settle outputs
+
+
+
+
+
 
 
 
@@ -73,6 +117,92 @@ int HPWH::runOneStep(double inletT, double drawVolume,
 
 return 0;
 } //end runOneStep
+
+
+void HPWH::updateTankTemps(double drawVolume_L, double inletT_C, double ambientT_C, double minutesPerStep)
+{
+//set up some useful variables for calculations
+double volPerNode_L = tankVolume_L/numNodes;
+double drawFraction;
+int wholeNodesToDraw;
+outletTemp_C = 0;
+
+//calculate how many nodes to draw (wholeNodesToDraw), and the remainder (drawFraction)
+drawFraction = drawVolume_L/volPerNode_L;
+wholeNodesToDraw = (int)std::floor(drawFraction);
+drawFraction -= wholeNodesToDraw;
+
+//move whole nodes
+if(wholeNodesToDraw > 0){				
+	for(int i = 0; i < wholeNodesToDraw; i++){
+		//add temperature of drawn nodes for outletT average
+		outletTemp_C += tankTemps_C[numNodes-1 - i];  
+	}
+
+	for(int i = numNodes-1; i >= 0; i--){
+		if(i > wholeNodesToDraw-1){
+			//move nodes up
+			tankTemps_C[i] = tankTemps_C[i - wholeNodesToDraw];
+		}
+		else{
+			//fill in bottom nodes with inlet water
+			tankTemps_C[i] = inletT_C;	
+		}
+	}
+}
+//move fractional node
+if(drawFraction > 0){
+	//add temperature for outletT average
+	outletTemp_C += drawFraction*tankTemps_C[numNodes - 1];
+	//move partial nodes up
+	for(int i = numNodes-1; i > 0; i--){
+		tankTemps_C[i] = tankTemps_C[i] *(1.0 - drawFraction) + tankTemps_C[i-1] * drawFraction;
+	}
+	//fill in bottom partial node with inletT
+	tankTemps_C[0] = tankTemps_C[0] * (1.0 - drawFraction) + inletT_C*drawFraction;
+}
+
+//fill in average outlet T
+outletTemp_C /= (wholeNodesToDraw + drawFraction);
+
+
+
+
+
+//Account for mixing at the bottom of the tank
+if(tankMixing == true && drawVolume_L > 0){
+	int mixedBelowNode = numNodes / 3;
+	double ave = 0;
+	
+	for(int i = 0; i < mixedBelowNode; i++){
+		ave += tankTemps_C[i];
+	}
+	ave /= mixedBelowNode;
+	
+	for(int i = 0; i < mixedBelowNode; i++){
+		tankTemps_C[i] += ((ave - tankTemps_C[i]) / 3.0);
+	}
+}
+
+//calculate standby losses
+//get average tank temperature
+double avgTemp = 0;
+for(int i = 0; i < numNodes; i++) avgTemp += tankTemps_C[i];
+avgTemp /= numNodes;
+
+//kJ's lost as standby in the current time step
+double standbyLosses_kJ = (tankUA_kJperHrC * (avgTemp - ambientT_C) * (minutesPerStep / 60.0));	
+standbyLosses_kWh = standbyLosses_kJ / 3600.0;
+
+//The effect of standby loss on temperature in each segment
+double lossPerNode_C = (standbyLosses_kJ / numNodes)    /    ((volPerNode_L * DENSITYWATER_kgperL) * CPWATER_kJperkgC);
+for(int i = 0; i < numNodes; i++) tankTemps_C[i] -= lossPerNode_C;
+
+
+}	//end updateTankTemps
+
+
+
 
 
 
--- dev/HPWH.hh
+++ dev/HPWH.hh
@@ -2,6 +2,13 @@
 #define HPWH_hh
 
 #include <string>
+using std::string;
+#include <cmath>
+
+
+#define DENSITYWATER_kgperL 0.998
+#define CPWATER_kJperkgC 4.181
+
 
 class HPWH
 {
@@ -19,7 +26,7 @@ public:
 	 * The return value is 0 for successful init, something else otherwise
 	 */
 
-	int HPWHinit_file(std::string configFile);
+	int HPWHinit_file(string configFile);
 	/* This function will load in a set of parameters from a file
 	 * The file name is the input - there should be at most one set of parameters per file
 	 * This is useful for testing new variations, and for the sort of variability
@@ -28,9 +35,9 @@ public:
 	 * The return value is 0 for successful init, something else otherwise
 	 */
 
-	int runOneStep(double inletT, double drawVolume, 
-					double ambientT, double externalT,
-					double DRstatus);
+	int runOneStep(double inletT_C, double drawVolume_L, 
+					double ambientT_C, double externalT_C,
+					double DRstatus, double minutesPerStep);
 	/* This function will progress the simulation forward in time by one step
 	 * all calculated outputs are stored in private variables and accessed through functions
 	 * 
@@ -38,9 +45,9 @@ public:
 	 */
 	 
 
-	int runNSteps(int N,  double inletT, double drawVolume, 
-					double ambientT, double externalT,
-					double DRstatus);
+	int runNSteps(int N,  double inletT_C, double drawVolume_L, 
+					double ambientT_C, double externalT_C,
+					double DRstatus, double minutesPerStep);
 	/* This function will progress the simulation forward in time by N steps
 	 * The calculated values will be summed or averaged, as appropriate, and 
 	 * then stored in the usual variables to be accessed through functions
@@ -64,17 +71,21 @@ public:
 	//get an array of the run time for each element, in order of element priority - 
 	//this may sum to more than 1 time step for concurrently running elements
 	
-	double getOutletTemp() const;
+	double getOutletTemp(string units) const;
 	//a function to get the outlet temperature - returns 0 when no draw occurs
-	double getEnergyRemovedFromEnvironment() const;
+	double getEnergyRemovedFromEnvironment(string units) const;
 	//get the total energy removed from the environment by all elements (not net energy - does not include standby)
-	double getStandbyLosses() const;
+	double getStandbyLosses(string units) const;
 	//get the amount of heat lost through the tank
 	 
 	 
 	 
 private:	
 	class Element;
+
+	void updateTankTemps(double draw, double inletT, double ambientT, double minutesPerStep);
+
+
 	
 	int numElements;
 	//how many elements this HPWH has
@@ -83,11 +94,27 @@ private:
 	
 	int numNodes;
 	//the number of nodes in the tank
-	double tankVolume;
-	//the volume in gallons of the tank
+	double tankVolume_L;
+	//the volume in liters of the tank
+	double tankUA_kJperHrC;
 	
 	
+	double *tankTemps_C;
+	//an array holding the temperature of each node - 0 is the bottom node, numNodes is the top
 	
+	
+
+	//Some outputs
+	double outletTemp_C;
+	//the temperature of the outlet water - taken from top of tank, 0 if no flow
+	double energyRemovedFromEnvironment_kWh;
+	//the total energy removed from the environment, to heat the water
+	double standbyLosses_kWh;
+	//the amount of heat lost to standby
+	
+	
+	//special variables for adding abilities
+	bool tankMixing;
 	
 	bool doTempDepression;
 	//whether the HPWH should track an alternate ambient temperature and 
@@ -95,10 +122,9 @@ private:
 	double locationTemperature;
 	//this is the special location temperature that stands in for the the 
 	//ambient temperature if you are doing temp. depression
-
-
-
+	
 };  //end of HPWH class
+
 
 
 
@@ -108,6 +134,9 @@ class HPWH::Element
 {
 friend class HPWH;
 public:
+	Element() {};
+	//default constructor, does not create a useful Element
+	
 	Element(HPWH *parentHPWH);
 	//constructor assigns a pointer to the hpwh creating this element 
 	
@@ -129,9 +158,21 @@ private:
 	HPWH *hpwh;
 	//the creator of the element, necessary to access HPWH variables
 	
+	//these are the element state/output variables
 	bool isEngaged;
 	//is the element running or not	
+	double runtime;
+	//this is the percentage of the step that the element was running
+	double energyInput;
+	//the energy used by the element
+	double energyOutput;
+	//the energy put into the water by the element
 
+
+
+
+	//these are the element property variables
+	
 	double condensity[12];
 	//The condensity function is always composed of 12 nodes.  
 	//It represents the location within the tank where heat will be distributed,
