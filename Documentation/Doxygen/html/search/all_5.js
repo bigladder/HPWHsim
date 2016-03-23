22c0b6ed55098ca972bf7214337db32552735b7f
nkvaltine <nkvaltine@ecotope.com>
2016-03-22 17:51:39 -0700

    update doxygen docs

--- Documentation/Doxygen/html/search/all_5.js
+++ Documentation/Doxygen/html/search/all_5.js
@@ -7,5 +7,6 @@ var searchData=
   ['hpwh',['HPWH',['../class_h_p_w_h.html',1,'HPWH'],['../class_h_p_w_h.html#a07cd672278715ee61604ae26137d16f7',1,'HPWH::HPWH()'],['../class_h_p_w_h.html#aa28907ae69a5f53e53bb14554c9af73f',1,'HPWH::HPWH(const HPWH &amp;hpwh)']]],
   ['hpwh_5fabort',['HPWH_ABORT',['../class_h_p_w_h.html#a727a9e272cf7ac3564ebd67bfb8ed063',1,'HPWH']]],
   ['hpwhinit_5ffile',['HPWHinit_file',['../class_h_p_w_h.html#a348c1c4a90ec1ee5e91c84ab8d3bc5b1',1,'HPWH']]],
-  ['hpwhinit_5fpresets',['HPWHinit_presets',['../class_h_p_w_h.html#a74b3a6c1352adb5d38e7c00ad5952f30',1,'HPWH']]]
+  ['hpwhinit_5fpresets',['HPWHinit_presets',['../class_h_p_w_h.html#a74b3a6c1352adb5d38e7c00ad5952f30',1,'HPWH']]],
+  ['hpwhinit_5frestank',['HPWHinit_resTank',['../class_h_p_w_h.html#ac092212830c4e023b1314845d79d5461',1,'HPWH::HPWHinit_resTank()'],['../class_h_p_w_h.html#a0b8ee9f3e4773ca140b50997ad1091cf',1,'HPWH::HPWHinit_resTank(double tankVol_L, double energyFactor, double upperPower_W, double lowerPower_W)']]]
 ];
--- Documentation/Doxygen/html/search/functions_9.js
+++ Documentation/Doxygen/html/search/functions_9.js
@@ -7,6 +7,7 @@ var searchData=
   ['setmessagecallback',['setMessageCallback',['../class_h_p_w_h.html#af20440c49f36a44b66b82d16b0b6a74b',1,'HPWH']]],
   ['setsetpoint',['setSetpoint',['../class_h_p_w_h.html#adb49b5846f9056602de57cb289e0a321',1,'HPWH::setSetpoint(double newSetpoint)'],['../class_h_p_w_h.html#ab7d91fead271ef907cf13450055cdb06',1,'HPWH::setSetpoint(double newSetpoint, UNITS units)']]],
   ['settanksize',['setTankSize',['../class_h_p_w_h.html#a48eb98ee5cef78dc29fbeaba57c37b2d',1,'HPWH']]],
+  ['setua',['setUA',['../class_h_p_w_h.html#a303ac307093da4ef292dc88f946d677f',1,'HPWH']]],
   ['setupasresistiveelement',['setupAsResistiveElement',['../class_h_p_w_h_1_1_heat_source.html#abc4bca8cd776b2d3fe82e5ed4ee679a5',1,'HPWH::HeatSource']]],
   ['setverbosity',['setVerbosity',['../class_h_p_w_h.html#a7c81dfc13de82ce9ca01300bde0d4b22',1,'HPWH']]],
   ['shouldheat',['shouldHeat',['../class_h_p_w_h_1_1_heat_source.html#ac60987b0b6c963c08d5a5fe34e8232e9',1,'HPWH::HeatSource']]],
