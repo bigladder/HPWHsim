fc223d1eb35ec1fd9fcd8cfe3afe550dc63aba80
nkvaltine <nkvaltine@ecotope.com>
2016-03-30 14:46:33 -0700

    add setpointFixed to file input, update doxygen, version to 1.2.1

--- Documentation/Doxygen/html/search/all_6.js
+++ Documentation/Doxygen/html/search/all_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['isengaged',['isEngaged',['../class_h_p_w_h_1_1_heat_source.html#a0c06eacf6cb6812126674dd3f646be61',1,'HPWH::HeatSource']]],
-  ['isnthheatsourcerunning',['isNthHeatSourceRunning',['../class_h_p_w_h.html#a587a39a680823886348b489e85f243e9',1,'HPWH']]]
+  ['isnthheatsourcerunning',['isNthHeatSourceRunning',['../class_h_p_w_h.html#a587a39a680823886348b489e85f243e9',1,'HPWH']]],
+  ['issetpointfixed',['isSetpointFixed',['../class_h_p_w_h.html#adeb9b796845880f192dbaf44ad1b9ffb',1,'HPWH']]]
 ];
