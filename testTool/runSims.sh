23781e68cf6af0e3a5ac6ec961ed617c6ce8c436
nkvaltine <nkvaltine@ecotope.com>
2016-05-17 14:44:33 -0700

    add all separate draw profile tests, and runSims.sh to run them

---
+++ testTool/runSims.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+model=worstCase
+model=GE2014
+
+for i in `seq 1 7`; do
+  for j in `seq 1 5`; do
+    ./testTool.x DrawProfileTest_${j}p${i}_24hr67 $model
+  done
+done
+
+
+./testTool.x slowFlowTest_0.25 $model
+./testTool.x slowFlowTest_0.5 $model
+./testTool.x slowFlowTest_0.75 $model
+./testTool.x slowFlowTest_1.0 $model
