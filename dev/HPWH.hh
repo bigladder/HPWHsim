7d7e5f82900df150c8fd0bf98792794f56adafbf
nkvaltine <nkvaltine@ecotope.com>
2016-05-20 15:34:21 -0700

    clean up a few things, increment maint version number

--- dev/HPWH.hh
+++ dev/HPWH.hh
@@ -20,7 +20,7 @@ class HPWH {
  public:
   static const int version_major = 1;
   static const int version_minor = 2;
-  static const int version_maint = 7;
+  static const int version_maint = 8;
 
 
   static const float DENSITYWATER_kgperL;
