f0e4c6c3602d39298943c812c7dc89383a0641cb
galanca <cesar.galan@colorado.edu>
2023-02-23 14:45:43 -0700

    Add interface library.

---
+++ cmake/compiler-flags.cmake
@@ -0,0 +1,17 @@
+add_library(hpwhsim_interface_library INTERFACE)
+
+target_compile_options(hpwhsim_interface_library INTERFACE
+$<$<CXX_COMPILER_ID:MSVC>: # Visual C++ (VS 2013)
+    /W4   # Warning level (default is W3)
+    #/WX  # Turn warnings into errors
+  >
+
+  $<$<CXX_COMPILER_ID:GNU,Clang,AppleClang>: # GCC and Clang
+    -Wall       # Turn on all warnings
+    -Wextra     # Turn on extra warnings
+    -Wpedantic  # Turn on warning not covered in Wall and Wextra
+    #-Werror    # Turn warnings into errors
+    -fpic       # Position Independent Code
+    -Wshadow
+  >
+)
