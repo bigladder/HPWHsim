9d24670882bda4992468ca5865412ccf148dbd62
Phil Ahrenkiel <Phil Ahrenkiel@DESKTOP-5PJG564>
2023-11-17 11:05:07 -0700

    Separate HeatingLogic header.

---
+++ src/HPWHHeatingLogics.hh
@@ -0,0 +1,113 @@
+
+#ifndef HPWHHeatingLogics_hh
+#define HPWHHeatingLogics_hh
+
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <functional>
+#include <memory>
+
+#include <cstdio>
+#include <cstdlib>   //for exit
+#include <vector>
+
+#include "HPWH.hh"
+
+struct HPWH::NodeWeight {
+	int nodeNum;
+	double weight;
+	NodeWeight(int n,double w): nodeNum(n),weight(w) {};
+
+	NodeWeight(int n): nodeNum(n),weight(1.0) {};
+};
+ 
+struct HPWH::HeatingLogic {
+public:
+	std::string description;
+	std::function<bool(double,double)> compare;
+
+	HeatingLogic(std::string desc,double decisionPoint_in,HPWH *hpwh_in,
+		std::function<bool(double,double)> c,bool isHTS):
+		description(desc),decisionPoint(decisionPoint_in),hpwh(hpwh_in),compare(c),
+		isEnteringWaterHighTempShutoff(isHTS)
+	{};
+
+	/**< checks that the input is all valid. */
+	virtual const bool isValid() = 0;
+	/**< gets the value for comparing the tank value to, i.e. the target SoC */
+	virtual const double getComparisonValue() = 0;
+	/**< gets the calculated value from the tank, i.e. SoC or tank average of node weights*/
+	virtual const double getTankValue() = 0;
+	/**< function to calculate where the average node for a logic set is. */
+	virtual const double nodeWeightAvgFract() = 0;
+	/**< gets the fraction of a node that has to be heated up to met the turnoff condition*/
+	virtual const double getFractToMeetComparisonExternal() = 0;
+
+	virtual int setDecisionPoint(double value) = 0;
+	double getDecisionPoint() { return decisionPoint; }
+	bool getIsEnteringWaterHighTempShutoff() { return isEnteringWaterHighTempShutoff; }
+
+protected:
+	double decisionPoint;
+	HPWH* hpwh;
+	bool isEnteringWaterHighTempShutoff;
+};
+
+struct HPWH::SoCBasedHeatingLogic: HeatingLogic {
+public:
+	SoCBasedHeatingLogic(std::string desc,double decisionPoint,HPWH *hpwh,
+		double hF = -0.05,double tM_C = 43.333,bool constMains = false,double mains_C = 18.333,
+		std::function<bool(double,double)> c = std::less<double>()):
+		HeatingLogic(desc,decisionPoint,hpwh,c,false),
+		hysteresisFraction(hF),tempMinUseful_C(tM_C),
+		useCostantMains(constMains),constantMains_C(mains_C)
+	{};
+	const bool isValid();
+
+	const double getComparisonValue();
+	const double getTankValue();
+	const double nodeWeightAvgFract();
+	const double getFractToMeetComparisonExternal();
+	const double getMainsT_C();
+	const double getTempMinUseful_C();
+	int setDecisionPoint(double value);
+	int setConstantMainsTemperature(double mains_C);
+
+private:
+	double tempMinUseful_C;
+	double hysteresisFraction;
+	bool useCostantMains;
+	double constantMains_C;
+};
+
+struct HPWH::TempBasedHeatingLogic: HeatingLogic {
+public:
+	TempBasedHeatingLogic(std::string desc,std::vector<NodeWeight> n,
+		double decisionPoint,HPWH *hpwh,bool a = false,
+		std::function<bool(double,double)> c = std::less<double>(),
+		bool isHTS = false):
+		HeatingLogic(desc,decisionPoint,hpwh,c,isHTS),
+		nodeWeights(n),isAbsolute(a)
+	{};
+
+	const bool isValid();
+
+	const double getComparisonValue();
+	const double getTankValue();
+	const double nodeWeightAvgFract();
+	const double getFractToMeetComparisonExternal();
+
+	int setDecisionPoint(double value);
+	int setDecisionPoint(double value,bool absolute);
+
+private:
+	const bool areNodeWeightsValid();
+
+	bool isAbsolute;
+	std::vector<NodeWeight> nodeWeights;
+};
+
+#endif
--- src/HPWHMain.hh
+++ src/HPWHMain.hh
@@ -268,105 +268,16 @@ public:
 		CSVOPT_IPUNITS
 	};
 
-	struct NodeWeight {
-		int nodeNum;
-		double weight;
-		NodeWeight(int n,double w): nodeNum(n),weight(w) {};
+	struct HeatingLogic;
 
-		NodeWeight(int n): nodeNum(n),weight(1.0) {};
-	};
-
-	struct HeatingLogic {
-	public:
-		std::string description;
-		std::function<bool(double,double)> compare;
-
-		HeatingLogic(std::string desc,double decisionPoint_in,HPWH *hpwh_in,
-			std::function<bool(double,double)> c,bool isHTS):
-			description(desc),decisionPoint(decisionPoint_in),hpwh(hpwh_in),compare(c),
-			isEnteringWaterHighTempShutoff(isHTS)
-		{};
-
-		/**< checks that the input is all valid. */
-		virtual const bool isValid() = 0;
-		/**< gets the value for comparing the tank value to, i.e. the target SoC */
-		virtual const double getComparisonValue() = 0;
-		/**< gets the calculated value from the tank, i.e. SoC or tank average of node weights*/
-		virtual const double getTankValue() = 0;
-		/**< function to calculate where the average node for a logic set is. */
-		virtual const double nodeWeightAvgFract() = 0;
-		/**< gets the fraction of a node that has to be heated up to met the turnoff condition*/
-		virtual const double getFractToMeetComparisonExternal() = 0;
-
-		virtual int setDecisionPoint(double value) = 0;
-		double getDecisionPoint() { return decisionPoint; }
-		bool getIsEnteringWaterHighTempShutoff() { return isEnteringWaterHighTempShutoff; }
-
-	protected:
-		double decisionPoint;
-		HPWH* hpwh;
-		bool isEnteringWaterHighTempShutoff;
-	};
-
-	struct SoCBasedHeatingLogic: HeatingLogic {
-	public:
-		SoCBasedHeatingLogic(std::string desc,double decisionPoint,HPWH *hpwh,
-			double hF = -0.05,double tM_C = 43.333,bool constMains = false,double mains_C = 18.333,
-			std::function<bool(double,double)> c = std::less<double>()):
-			HeatingLogic(desc,decisionPoint,hpwh,c,false),
-			hysteresisFraction(hF),tempMinUseful_C(tM_C),
-			useCostantMains(constMains),constantMains_C(mains_C)
-		{};
-		const bool isValid();
-
-		const double getComparisonValue();
-		const double getTankValue();
-		const double nodeWeightAvgFract();
-		const double getFractToMeetComparisonExternal();
-		const double getMainsT_C();
-		const double getTempMinUseful_C();
-		int setDecisionPoint(double value);
-		int setConstantMainsTemperature(double mains_C);
-
-	private:
-		double tempMinUseful_C;
-		double hysteresisFraction;
-		bool useCostantMains;
-		double constantMains_C;
-	};
-
-	struct TempBasedHeatingLogic: HeatingLogic {
-	public:
-		TempBasedHeatingLogic(std::string desc,std::vector<NodeWeight> n,
-			double decisionPoint,HPWH *hpwh,bool a = false,
-			std::function<bool(double,double)> c = std::less<double>(),
-			bool isHTS = false):
-			HeatingLogic(desc,decisionPoint,hpwh,c,isHTS),
-			nodeWeights(n),isAbsolute(a)
-		{};
-
-		const bool isValid();
-
-		const double getComparisonValue();
-		const double getTankValue();
-		const double nodeWeightAvgFract();
-		const double getFractToMeetComparisonExternal();
-
-		int setDecisionPoint(double value);
-		int setDecisionPoint(double value,bool absolute);
-
-	private:
-		const bool areNodeWeightsValid();
-
-		bool isAbsolute;
-		std::vector<NodeWeight> nodeWeights;
-	};
-
-	std::shared_ptr<HPWH::SoCBasedHeatingLogic> shutOffSoC(std::string desc,double targetSoC,double hystFract,double tempMinUseful_C,
+	struct SoCBasedHeatingLogic;
+	std::shared_ptr<SoCBasedHeatingLogic> shutOffSoC(std::string desc,double targetSoC,double hystFract,double tempMinUseful_C,
 		bool constMains,double mains_C);
-	std::shared_ptr<HPWH::SoCBasedHeatingLogic> turnOnSoC(std::string desc,double targetSoC,double hystFract,double tempMinUseful_C,
+	std::shared_ptr<SoCBasedHeatingLogic> turnOnSoC(std::string desc,double targetSoC,double hystFract,double tempMinUseful_C,
 		bool constMains,double mains_C);
 
+	struct NodeWeight;
+	struct TempBasedHeatingLogic;
 	std::shared_ptr<TempBasedHeatingLogic> topThird(double decisionPoint);
 	std::shared_ptr<TempBasedHeatingLogic> topThird_absolute(double decisionPoint);
 	std::shared_ptr<TempBasedHeatingLogic> bottomThird(double decisionPoint);
@@ -996,47 +907,5 @@ private:
 	std::vector<HPWH::NodeWeight> getNodeWeightRange(double bottomFraction,double topFraction);
 
 };  //end of HPWH class
-
-
-// a few extra functions for unit converesion
-inline double dF_TO_dC(double temperature) { return (temperature*5.0/9.0); }
-inline double F_TO_C(double temperature) { return ((temperature - 32.0)*5.0/9.0); }
-inline double C_TO_F(double temperature) { return (((9.0/5.0)*temperature) + 32.0); }
-inline double KWH_TO_BTU(double kwh) { return (3412.14 * kwh); }
-inline double KWH_TO_KJ(double kwh) { return (kwh * 3600.0); }
-inline double BTU_TO_KWH(double btu) { return (btu / 3412.14); }
-inline double BTUperH_TO_KW(double btu) { return (btu / 3412.14); }
-inline double KW_TO_BTUperH(double kw) { return (kw * 3412.14); }
-inline double W_TO_BTUperH(double w) { return (w * 3.41214); }
-inline double KJ_TO_KWH(double kj) { return (kj/3600.0); }
-inline double BTU_TO_KJ(double btu) { return (btu * 1.055); }
-inline double GAL_TO_L(double gallons) { return (gallons * 3.78541); }
-inline double L_TO_GAL(double liters) { return (liters / 3.78541); }
-inline double L_TO_FT3(double liters) { return (liters / 28.31685); }
-inline double UAf_TO_UAc(double UAf) { return (UAf * 1.8 / 0.9478); }
-inline double GPM_TO_LPS(double gpm) { return (gpm * 3.78541 / 60.0); }
-inline double LPS_TO_GPM(double lps) { return (lps * 60.0 / 3.78541); }
-
-inline double FT_TO_M(double feet) { return (feet / 3.2808); }
-inline double FT2_TO_M2(double feet2) { return (feet2 / 10.7640); }
-
-inline double MIN_TO_SEC(double minute) { return minute * 60.; }
-inline double MIN_TO_HR(double minute) { return minute / 60.; }
-
-inline HPWH::DRMODES operator|(HPWH::DRMODES a,HPWH::DRMODES b)
-{
-	return static_cast<HPWH::DRMODES>(static_cast<int>(a) | static_cast<int>(b));
-}
-
-template< typename T> inline bool aboutEqual(T a,T b) { return fabs(a - b) < HPWH::TOL_MINVALUE; }
-
-// resampling utility functions
-double getResampledValue(const std::vector<double> &values,double beginFraction,double endFraction);
-bool resample(std::vector<double> &values,const std::vector<double> &sampleValues);
-inline bool resampleIntensive(std::vector<double> &values,const std::vector<double> &sampleValues)
-{
-	return resample(values,sampleValues);
-}
-bool resampleExtensive(std::vector<double> &values,const std::vector<double> &sampleValues);
 
 #endif
