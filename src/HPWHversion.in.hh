8655a00b5b0a7c3ab4dd36a1339399d9f6050f21
Phil Ahrenkiel <Phil Ahrenkiel@DESKTOP-5PJG564>
2023-11-20 09:53:13 -0700

    Remove unused includes.

--- src/HPWHversion.in.hh
+++ src/HPWHversion.in.hh
@@ -1,17 +1,9 @@
+/*
+* Version control
+*/
+
 #ifndef HPWHversion_hh
 #define HPWHversion_hh
-
-#include <string>
-#include <sstream>
-#include <cmath>
-#include <iostream>
-#include <iomanip>
-#include <functional>
-#include <memory>
-
-#include <cstdio>
-#include <cstdlib>   //for exit
-#include <vector>
 
 #define HPWHVRSN_MAJOR @HPWHsim_VRSN_MAJOR@
 #define HPWHVRSN_MINOR @HPWHsim_VRSN_MINOR@
