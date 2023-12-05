6c911157154fd6f4531f8a4a09335990d7c07ff0
Phil Ahrenkiel <Phil Ahrenkiel@DESKTOP-5PJG564>
2023-12-05 12:24:26 -0700

    Enable writing outputT.

--- src/HPWH.cc
+++ src/HPWH.cc
@@ -795,7 +795,6 @@ void HPWH::printTankTemps() {
 	msg(ss.str().c_str());
 }
 
-
 // public members to write to CSV file
 int HPWH::WriteCSVHeading(FILE* outFILE,const char* preamble,int nTCouples,int options) const {
 
@@ -812,6 +811,9 @@ int HPWH::WriteCSVHeading(FILE* outFILE,const char* preamble,int nTCouples,int o
 	for(int iTC = 0; iTC < nTCouples; iTC++) {
 		fprintf(outFILE,",tcouple%d (%s)",iTC + 1,doIP ? "F" : "C");
 	}
+
+	if (options & HPWH::CSVOPT_WRITE_OUTLET_T)
+		fprintf(outFILE,", toutlet (%s)",doIP ? "F" : "C");
 
 	fprintf(outFILE,"\n");
 
@@ -835,6 +837,14 @@ int HPWH::WriteCSVRow(FILE* outFILE,const char* preamble,int nTCouples,int optio
 		fprintf(outFILE,",%0.2f",getNthSimTcouple(iTC + 1,nTCouples,doIP ? UNITS_F : UNITS_C));
 	}
 
+	if (options & HPWH::CSVOPT_WRITE_OUTLET_T) {
+		if (options & HPWH::CSVOPT_IS_DRAWING) {
+			fprintf(outFILE,",%0.2f",doIP ? C_TO_F(outletTemp_C) : outletTemp_C);
+		}
+		else {
+			fprintf(outFILE,",");
+		}
+	}
 	fprintf(outFILE,"\n");
 
 	return 0;
