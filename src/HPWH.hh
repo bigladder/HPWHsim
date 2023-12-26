9710484054ec4f281d1750ef34c066dcb4e94841
Phil Ahrenkiel <Phil Ahrenkiel@DESKTOP-5PJG564>
2023-12-26 09:37:55 -0700

    Correct format.

--- src/HPWH.hh
+++ src/HPWH.hh
@@ -41,8 +41,8 @@ class HPWH
         200; /**< this is the maximum length for a debuging output string */
 
     static const double DENSITYWATER_kgperL; /// mass density of water
-    static const double KWATER_WpermC; /// thermal conductivity of water
-    static const double cPWATER_kJperkgC; /// specific heat capcity of water
+    static const double KWATER_WpermC;       /// thermal conductivity of water
+    static const double cPWATER_kJperkgC;    /// specific heat capcity of water
     static const double TOL_MINVALUE; /**< any amount of heat distribution less than this is reduced
                                          to 0 this saves on computations */
 
