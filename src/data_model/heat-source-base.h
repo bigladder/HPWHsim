d8b2247c20d34c5afbf2b46a0dae8328ca679de4
Phil Ahrenkiel <spahrenk@gmail.com>
2024-05-16 09:00:56 -0600

    Add heatsourcebase.

---
+++ src/data_model/heat-source-base.h
@@ -0,0 +1,22 @@
+#ifndef HEATSOURCEBASE_H_
+#define HEATSOURCEBASE_H_
+#include <ashrae205.h>
+
+#include <string>
+#include <vector>
+#include <enum-info.h>
+#include <courier/courier.h>
+#include <nlohmann/json.hpp>
+
+/// @note  This class has been auto-generated. Local changes will not be saved!
+
+namespace hpwh_data_model
+{
+struct HeatSourceBase
+{
+    virtual void from_json(const nlohmann::json& j) = 0;
+    virtual ~HeatSourceBase() = default;
+};
+} // namespace hpwh_data_model
+
+#endif
