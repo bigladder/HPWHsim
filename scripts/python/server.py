e8942b474ef30044be522afaef535ab4702b2839
Phil Ahrenkiel <spahrenk@gmail.com>
2024-07-21 10:46:03 -0600

    Rename and organize.

--- scripts/python/server.py
+++ scripts/python/server.py
@@ -1,6 +1,6 @@
 import http.server
 import socketserver
-import run_plot
+import main
 
 # in scripts/python
 # poetry run python server.py
@@ -11,19 +11,19 @@ PORT = 8000
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         print(self.path)
-        if self.path == '/run_function':
+        if self.path == '/run_python_function':
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
            
-            result = my_python_function() 
+            result = python_function() 
             print(result)
             #self.wfile.write(result)
         else:
             super().do_GET()
 
-def my_python_function():
-    return run_plot.main_fnc()
+def python_function():
+    return main.main()
 
 if __name__ == "__main__":
     with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
