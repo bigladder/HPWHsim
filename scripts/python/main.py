5b94ad073c562c9203fca2f21821e7ee93c1d910
Phil Ahrenkiel <spahrenk@gmail.com>
2024-07-21 11:06:55 -0600

    Improve main.

--- scripts/python/main.py
+++ scripts/python/main.py
@@ -8,7 +8,7 @@ from plot import plot
 
 def main():
 	#
-	orig_path = str(Path.cwd())
+	orig_dir = str(Path.cwd())
 	os.chdir('../..')
 	repo_path = str(Path.cwd())
 	print("path is " + repo_path)
@@ -16,23 +16,23 @@ def main():
 	model_spec = 'File'
 	model_name = 'AeroTherm2023'
 	test_name = 'RE2H50_UEF67'
-	dest_name = 'aplot.html'
+	plot_name = 'aplot.html'
+	measurements_name = 'measurements.csv'
+	output_name = test_name + '_' + model_spec + '_' + model_name + ".csv"
 
-	output_path = os.path.join(repo_path, "build", "test", "output")
-	simulate(repo_path, model_spec, model_name, test_name, output_path)
+	test_dir = os.path.join(repo_path, "test")
+	output_dir = os.path.join(repo_path, "build", "test", "output")
+	simulate(repo_path, model_spec, model_name, test_name, output_dir)
 
 	#
-	test_path = os.path.join(repo_path, "test")
-	measured_path = os.path.join(test_path, test_name, "measurements.csv")
-
-	build_path = os.path.join(repo_path, "build", "test", "output")
-	simulated_path=os.path.join(build_path, test_name + '_' + model_spec + '_' + model_name + ".csv")
-
-	output_path = os.path.join(output_path, dest_name)
+	measured_path = os.path.join(test_dir, test_name, measurements_name)
+	simulated_path = os.path.join(output_dir, output_name)
+	plot_path = os.path.join(output_dir, plot_name)
 
 	print(measured_path)
 	print(simulated_path)
-	print(output_path)
-	plot(measured_path, simulated_path, output_path)
+	print(plot_path)
+
+	plot(measured_path, simulated_path, plot_path)
 	
-	os.chdir(orig_path)
+	os.chdir(orig_dir)
