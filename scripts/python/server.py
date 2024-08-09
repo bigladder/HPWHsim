46383f702c0836d1b1e8928ada53d1808a624a96
Phil Ahrenkiel <spahrenk@gmail.com>
2024-08-09 08:16:38 -0600

    Improve tabs.

--- scripts/python/server.py
+++ scripts/python/server.py
@@ -12,7 +12,7 @@ PORT = 8000
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path.startswith('/run_python_function'):
+        if self.path.startswith('/test_and_plot'):
             query_components = urlparse.parse_qs(urlparse.urlparse(self.path).query)
             model_spec = query_components.get('arg1', [None])[0]
             model_name = query_components.get('arg2', [None])[0]
@@ -20,7 +20,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
             plot_name = query_components.get('arg4', [None])[0]
             measurements_name = query_components.get('arg5', [None])[0]
 
-            result = python_function(model_spec, model_name, test_name, plot_name, measurements_name)
+            result = test_and_plot(model_spec, model_name, test_name, plot_name, measurements_name)
 
             self.send_response(200)
             self.send_header("Content-type", "text/html")
@@ -31,10 +31,8 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         else:
             super().do_GET()
 
-
-def python_function(model_spec, model_name, test_name, plot_name, measurements_name):
+def test_and_plot(model_spec, model_name, test_name, plot_name, measurements_name):
     return main.main(model_spec, model_name, test_name, plot_name, measurements_name)
-
 
 if __name__ == "__main__":
     with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
