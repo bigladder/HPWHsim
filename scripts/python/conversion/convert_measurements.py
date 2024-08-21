beb05b786cdf6bc6f33c6dd2125ee079f293962f
Phil Ahrenkiel <spahrenk@gmail.com>
2024-08-21 08:25:11 -0600

    Omit zero flowrate columns.

--- scripts/python/conversion/convert_measurements.py
+++ scripts/python/conversion/convert_measurements.py
@@ -5,6 +5,8 @@ out_data_folder = '../../test/RE2H65_UEF50/'
 in_file_name = in_data_folder + 'RE2H65_UEF50.csv'
 out_file_name = out_data_folder + 'measurements_test.csv'
 
+iColOutletT = 12
+iColFlowRate = 13
 # load data
 in_file = open(in_file_name, 'r')
 Lines = in_file.readlines()
@@ -25,12 +27,18 @@ for line in Lines:
 	else:			
 			columns = line.split(',')
 			first = True
+			iCol = 0
 			for column in columns:
 				if first:
 					line_out = column
 				else:
+					flowRate = float(columns[iColFlowRate].strip('\n'))
+					if flowRate == 0:
+						if iCol == iColOutletT or iCol == iColFlowRate:
+							column = ""							
 					line_out = line_out + "," + column
-				first = False	
+				first = False
+				iCol = iCol + 1
 			line_out = f"{line_out},{imin}"
 			if j > 5:
 				out_file.writelines(line_out+"\n")
