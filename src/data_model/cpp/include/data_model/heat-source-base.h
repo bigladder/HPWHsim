c7a92a8235fb6fde50757fdfccf34ccc3ae403ae
Phil Ahrenkiel <spahrenk@gmail.com>
2024-10-10 10:53:18 -0600

    Adjust test for initialT; format.

--- src/data_model/cpp/include/data_model/heat-source-base.h
+++ src/data_model/cpp/include/data_model/heat-source-base.h
@@ -1,7 +1,8 @@
 #ifndef HEATSOURCEBASE_H_
 #define HEATSOURCEBASE_H_
-struct HeatSourceBase {
-	virtual void initialize(const nlohmann::json& j) = 0;
+struct HeatSourceBase
+{
+    virtual void initialize(const nlohmann::json& j) = 0;
     virtual ~HeatSourceBase() = default;
 };
 #endif
