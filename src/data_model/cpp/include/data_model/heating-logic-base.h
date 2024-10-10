2c0527c746fd5c8aa33254ae301dfed0aa87565f
Phil Ahrenkiel <spahrenk@gmail.com>
2024-10-10 10:53:18 -0600

    Adjust test for initialT; format.

--- src/data_model/cpp/include/data_model/heating-logic-base.h
+++ src/data_model/cpp/include/data_model/heating-logic-base.h
@@ -1,7 +1,8 @@
 #ifndef HEATINGLOGICBASE_H_
 #define HEATINGLOGICBASE_H_
-struct HeatingLogicBase {
-	virtual void initialize(const nlohmann::json& j) = 0;
+struct HeatingLogicBase
+{
+    virtual void initialize(const nlohmann::json& j) = 0;
     virtual ~HeatingLogicBase() = default;
 };
 #endif
