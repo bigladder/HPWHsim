4b6948a5fcd6401338223a4eb9a0dc90bb1a058f
Phil Ahrenkiel <spahrenk@gmail.com>
2024-10-23 12:29:05 -0600

    Apply changes from aetheneum.

--- cmake/initialize-submodules.cmake
+++ cmake/initialize-submodules.cmake
@@ -1,39 +1,64 @@
-#update the named submodule
+macro(add_submodule submodule_name)
+    # Clone a submodule and add it's subdirectory to the build (if the corresponding target hasn't already been added)
+    set(options) # None
+    set(one_value_args
+            TARGET_NAME           # Specify if target name is different from the submodule name
+            PARENT_SUBMODULE_PATH # Specify if you want to leverage a submodule outside of this project's vendor directory
+            )
+    set(multi_value_args
+            CACHE_ON # Set options from submodule as "ON" in the cache
+            CACHE_OFF # Set options from submodule as "OFF" in the cache
+            MARK_AS_ADVANCED # Mark certain options from the submodule project as advanced (to hide from higher level CMake GUI)
+            )
+    cmake_parse_arguments(add_${submodule_name}_args "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
-#attempt to add the submodule
-#optional argument is module path
-macro(add_submodule module_name)
+    if (DEFINED add_${submodule_name}_args_TARGET_NAME)
+        set(target_name ${add_${submodule_name}_args_TARGET_NAME})
+    else ()
+        set(target_name "${submodule_name}")
+    endif ()
 
-    set(Args ${ARGN})
-    list(LENGTH Args NumArgs)
+    if (DEFINED add_${submodule_name}_args_PARENT_SUBMODULE_PATH)
+        set(submodule_path "${add_${submodule_name}_args_PARENT_SUBMODULE_PATH}/vendor/${submodule_name}")
+    else ()
+        set(submodule_path "${CMAKE_CURRENT_SOURCE_DIR}/${submodule_name}")
+    endif ()
 
-    if(NumArgs GREATER 0)
-        set(target_name "${ARGV1}")
-    else()
-        set(target_name "${module_name}")
-    endif()
-
-    set(module_path "${CMAKE_CURRENT_SOURCE_DIR}/${module_name}")
-    if(NumArgs GREATER 1)
-        set(module_path "${ARGV2}/vendor/${module_name}")
-        set(is_submodule false)
-    else()
-        set(is_submodule true)
-    endif()
-
-    if(${is_submodule} AND GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
-        message(STATUS "Updating submodule \"${module_name}\"")
-        execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive "${module_path}"
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    # Clone repository
+    if (GIT_FOUND AND NOT EXISTS "${submodule_path}/.git")
+        message(STATUS "Cloning submodule \"${submodule_name}\"")
+        execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init ${submodule_path}
+                WORKING_DIRECTORY ${submodule_path}
                 RESULT_VARIABLE GIT_SUBMOD_RESULT)
-        if(NOT GIT_SUBMOD_RESULT EQUAL "0")
-            message(FATAL_ERROR "Unable to update submodule \"${module_name}\"")
-        endif()
-    endif()
+        if (NOT GIT_SUBMOD_RESULT EQUAL "0")
+            message(FATAL_ERROR "Unable to update submodule \"${submodule_name}\"")
+        endif ()
+    endif ()
 
-    if (NOT TARGET ${target_name} AND (EXISTS "${module_path}"))
-        message(STATUS "Adding subdirectory \"${module_path}\"")
-        add_subdirectory(${module_path})
-    endif()
+    # Add subdirectory
+    if (NOT TARGET ${target_name} AND (EXISTS "${submodule_path}"))
+        add_subdirectory(${submodule_path})
+    endif ()
+
+    # Cache on
+    if (DEFINED add_${submodule_name}_args_CACHE_ON)
+        foreach (variable ${add_${submodule_name}_args_CACHE_ON})
+            set(variable ON CACHE BOOL "" FORCE)
+        endforeach ()
+    endif ()
+
+    # Cache off
+    if (DEFINED add_${submodule_name}_args_CACHE_OFF)
+        foreach (variable ${add_${submodule_name}_args_CACHE_OFF})
+            set(variable OFF CACHE BOOL "" FORCE)
+        endforeach ()
+    endif ()
+
+    # Mark as advanced
+    if (DEFINED add_${submodule_name}_args_MARK_AS_ADVANCED)
+        foreach (variable ${add_${submodule_name}_args_MARK_AS_ADVANCED})
+            mark_as_advanced(variable)
+        endforeach ()
+    endif ()
 
 endmacro()
