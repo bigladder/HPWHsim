a6c46bf5816fc64c393590c4b71a17ff736a20fd
Phil Ahrenkiel <spahrenk@gmail.com>
2024-10-24 10:32:33 -0600

    Move generated code to vendor.

--- src/data_model/cpp/include/data_model/heat-source-base.h
+++
@@ -1,8 +0,0 @@
-#ifndef HEATSOURCEBASE_H_
-#define HEATSOURCEBASE_H_
-struct HeatSourceBase
-{
-    virtual void initialize(const nlohmann::json& j) = 0;
-    virtual ~HeatSourceBase() = default;
-};
-#endif
