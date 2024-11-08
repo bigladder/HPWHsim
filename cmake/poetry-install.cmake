02158473201be66f177cbf561b8bbcc50be0c48f
Phil Ahrenkiel <spahrenk@gmail.com>
2024-11-08 10:51:08 -0700

    Use RESULT.

--- cmake/poetry-install.cmake
+++ cmake/poetry-install.cmake
@@ -2,17 +2,11 @@
 
 macro(poetry_install pythonScriptDir)
 
-    # execute 'poetry install'
-    #set(pythonScriptDir "${PROJECT_SOURCE_DIR}/scripts/python")
-    if (NOT ${PROJECT_NAME}_PYTHON_SETUP)
-        execute_process(
-                COMMAND poetry install
-                WORKING_DIRECTORY "${pythonScriptDir}"
-                RESULT_VARIABLE result0
-                ERROR_VARIABLE errors0
-                OUTPUT_VARIABLE outputs0)
-
-        set(${PROJECT_NAME}_PYTHON_SETUP TRUE CACHE BOOL "Python environment is already setup" FORCE)
-    endif ()
+    execute_process(
+            COMMAND poetry install
+            WORKING_DIRECTORY "${pythonScriptDir}"
+            RESULT_VARIABLE result
+            ERROR_VARIABLE errors
+            OUTPUT_VARIABLE outputs)
 
 endmacro()
