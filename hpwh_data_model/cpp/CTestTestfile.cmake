72623ba97b97772667d27d4bb620839da157f395
Phil Ahrenkiel <spahrenk@gmail.com>
2025-01-22 09:46:08 -0700

    Update data model; organize Condenser.

--- hpwh_data_model/cpp/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/phil-ahrenkiel/Documents/GitHub/HPWHsim/hpwh_data_model/cpp
-# Build directory: /Users/phil-ahrenkiel/Documents/GitHub/HPWHsim/build/hpwh_data_model/cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("vendor")
