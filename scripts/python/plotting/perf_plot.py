ad367e631d295cb77cddaf5372829da0fb3372dc
Phil Ahrenkiel <spahrenk@gmail.com>
2025-01-27 14:21:02 -0700

    Test value retention.

--- scripts/python/plotting/perf_plot.py
+++ scripts/python/plotting/perf_plot.py
@@ -21,9 +21,9 @@ class PerfPlotter:
 		self.have_data = False
 		self.is_central = False
 		self.i3 = 0
+		self.ivar = 1
 		
 		self.variables = ['InputPower', 'HeatingCapacity', 'COP']
-		self.variable = self.variables[1]
 		
 	def get_slice(self):
 		self.Pins = []
@@ -52,7 +52,9 @@ class PerfPlotter:
 			with open(model_path) as json_data:
 				self.data = json.load(json_data)
 		except:
+			print("failed to load")
 			return
+
 
 		if "integrated_system" in self.data:
 			self.is_central = False
@@ -74,16 +76,12 @@ class PerfPlotter:
 		grid_vars = self.perf_map["grid_variables"]
 		lookup_vars = self.perf_map["lookup_variables"]
 	
-		self.T1s = np.array(grid_vars["evaporator_environment_dry_bulb_temperature"])
+		self.T1s = np.array(grid_vars["evaporator_environment_dry_bulb_temperature"]) - 273.15
 		if self.is_central:
-			self.T2s = np.array(grid_vars["condenser_entering_temperature"])
-			self.T3s = np.array(grid_vars["condenser_leaving_temperature"])
+			self.T2s = np.array(grid_vars["condenser_entering_temperature"]) - 273.15
+			self.T3s = np.array(grid_vars["condenser_leaving_temperature"]) - 273.15
 		else:
-			self.T2s = np.array(grid_vars["heat_source_temperature"])
-		
-		to_C = lambda x: x - 273.15
-		self.T1s -= 273.15
-		self.T2s -= 273.15
+			self.T2s = np.array(grid_vars["heat_source_temperature"]) - 273.15
 
 		self.vPins = np.array(lookup_vars["input_power"])
 		self.vPouts = np.array(lookup_vars["heating_capacity"])
@@ -94,26 +92,37 @@ class PerfPlotter:
 			i = i + 1
 			
 		self.i3 = 0 if not self.is_central else math.floor(np.size(self.T3s) / 2)
+
 		self.get_slice()
+
  							   
 	def draw(self):	
-		if self.variable == self.variables[0]:
+		if self.ivar == 0:
 			zPlot = self.Pins
-		elif self.variable == self.variables[1]:
+		elif self.ivar == 1:
 			zPlot = self.Pouts
 		else:
 			zPlot = self.COPs	
 		
 		self.fig = go.Figure(data =
-										 go.Contour(z = zPlot, x = self.T1s, y = self.T2s))
-		self.fig.update_layout()
-		return self
+										 go.Contour(z = zPlot, x = self.T1s, y = self.T2s,
+											contours=dict(
+					            coloring ='heatmap',
+					            showlabels = True, # show labels on contours
+					            labelfont = dict( # label font properties
+					                size = 14,
+					                color = 'black',
+            					)
+											)))
+		
+		yaxis_title = "condenser inlet temperature (\u00B0C)" if self.is_central else "condenser temperature (C)"
+		self.fig.update_layout({
+    	"xaxis": {"title_text": "environment temperature (\u00B0C)", 'title_font': {"size": 14, "color": "black"}},
+    	"yaxis": {"title_text": yaxis_title, 'title_font': {"size": 14, "color": "black"}}
+			}
+			)
 
-def plot(model_path):
-	plotter = PerfPlotter()
-	plotter.read_data(model_path)
-	plotter.draw()
-	return plotter
+		return self
 
 def write_plot(model_path):
 	plotter = PerfPlotter()
--- scripts/python/plotting/perf_proc.py
+++ scripts/python/plotting/perf_proc.py
@@ -12,15 +12,23 @@ from scipy.optimize import least_squares
 import numpy as np
 import math
 import time
-from perf_plot import plot
+from perf_plot import PerfPlotter
 import multiprocessing as mp
 from pathlib import Path
 
 def perf_proc(plotter):
 	
 	perf_proc.plotter = plotter
-	
 	perf_proc.plotter.fig.update_layout(clickmode='event+select')
+	perf_proc.outletTs = []
+	perf_proc.show_outletTs = perf_proc.plotter.is_central
+	if perf_proc.show_outletTs:
+		i = 0
+		for outletT in perf_proc.plotter.T3s:
+			perf_proc.outletTs.append({'label': f"{outletT:.2f} \u00B0C", 'value': i})
+			i = i + 1
+	else:
+		perf_proc.outletTs = [{'label': "none", 'value': 0}]
 		
 	external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -39,21 +47,49 @@ def perf_proc(plotter):
 												 		{'label': 'Heating Capacity (W)', 'value': 1},
 														{'label': 'COP', 'value': 2}],
 														value = 1, 
-														id='display-dropdown'),
+														id='display-dropdown',
+														style={'width': '50%'},
+														clearable=False),
 	
+		html.Div(
+			[
+				html.Label("outlet temperature (\u00B0C)", htmlFor="outletT-dropdown"),
+				dcc.Dropdown(options = perf_proc.outletTs,
+																value = perf_proc.plotter.i3, 
+																id='outletT-dropdown',
+																style={'width': '50%'},
+																clearable=False,
+																)
+				
+				],
+				hidden = not(perf_proc.show_outletTs)
+				),
+														
 		dcc.Graph(id='perf-graph', figure=perf_proc.plotter.fig, style ={'width': '1200px', 'height': '800px', 'display': 'block'} )
 			
 	]
 
 	@callback(
-			Output('perf-graph', 'figure'),
+			Output('perf-graph', 'figure', allow_duplicate=True),
 			Input('display-dropdown', 'value'),
 			prevent_initial_call=True
 		)
 	def select_value(value):
-		perf_proc.plotter.variable = perf_proc.plotter.variables[value]
+		perf_proc.plotter.ivar = value
 		perf_proc.plotter.draw()
 		return perf_proc.plotter.fig
+	
+	@callback(
+				Output('perf-graph', 'figure', allow_duplicate=True),
+				Input('outletT-dropdown', 'value'),
+				prevent_initial_call=True
+			)
+	def select_outletT(value):
+		perf_proc.plotter.i3 = value
+		perf_proc.plotter.get_slice()
+		perf_proc.plotter.draw()
+		return perf_proc.plotter.fig
+
 	
 	app.run(debug=True, use_reloader=False, port = perf_proc.port_num)
 
@@ -69,21 +105,25 @@ def launch_perf_plot(model_name):
 	os.chdir(orig_dir)
 
 	model_path = os.path.join(abs_repo_test_dir, "models_json", model_name + ".json") 
-	print(model_path)
 	
 	print("creating plot...")
-	plotter = plot(model_path)
-	time.sleep(1)
+	plotter = PerfPlotter()
+	plotter.read_data(model_path)
 	
 	if launch_perf_plot.proc != -1:
 		print("killing current dash for plotting performance...")
 		launch_perf_plot.proc.kill()
 		time.sleep(1)
-		
-	launch_perf_plot.proc = mp.Process(target=perf_proc, args=(plotter, ), name='perf-proc')
+	
+	plotter.draw()
 	time.sleep(1)
+	
+	launch_perf_plot.proc = mp.Process(target=perf_proc, args=(plotter, ), name='perf-proc')
 	print("launching dash for plotting performance...")
+	time.sleep(1)
+
 	launch_perf_plot.proc.start()
+	print(launch_perf_plot.proc)
 	time.sleep(2)
 	   
 	results = {}
@@ -91,3 +131,4 @@ def launch_perf_plot(model_name):
 	return results
 
 launch_perf_plot.proc = -1
+
