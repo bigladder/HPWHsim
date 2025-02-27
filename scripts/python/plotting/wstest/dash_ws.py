bafe7dc1982ee72d99626c0425da5371b0525de2
Phil Ahrenkiel <spahrenk@gmail.com>
2025-02-27 13:34:15 -0700

    Fix full duplexing.

--- scripts/python/plotting/wstest/dash_ws.py
+++ scripts/python/plotting/wstest/dash_ws.py
@@ -7,13 +7,18 @@ from dash_extensions import WebSocket
 app = DashProxy(prevent_initial_callbacks=True)
 app.layout = html.Div([
     dcc.Input(id="input", autoComplete="off"), html.Div(id="message"),
-    WebSocket(url="ws://localhost:8600", id="ws")
+    WebSocket(url="ws://localhost:8600", id="ws"),
+    html.Button("send", id='send-btn', n_clicks=0),
 ])
 
 @app.callback(Output("ws", "send"), [Input("input", "value")])
 def send(value):
     print("sent from dash app")
     return value
+
+@app.callback(Output("input", "value"), Input("send-btn", "n_clicks"))
+def send_btn(n_clicks):
+    return n_clicks
 
 @app.callback(Output("message", "children"), [Input("ws", "message")])
 def message(e):
