283eff70bed39df315521031ae9c767f5873c13b
Phil Ahrenkiel <spahrenk@gmail.com>
2025-03-05 09:49:40 -0700

    Improve codegen.

--- scripts/python/data_model/build_data_model.py
+++ scripts/python/data_model/build_data_model.py
@@ -1,13 +1,14 @@
 # 'poetry update lattice', if needed
 # 'poetry run python build_data_model.py'
 
-# Generates hpwh-data-model source code based on schema in 'data_model'dir'
+# Generates hpwh-data-model source code based on schema in 'data_model_dir'
 # into 'src_out_dir'.
 
 from lattice import Lattice
 from pathlib import Path
 import os
 import sys
+import time
 
 def generate(repo_dir, data_model_dir, gen_out_dir):
 
@@ -17,13 +18,23 @@ def generate(repo_dir, data_model_dir, gen_out_dir):
 	working_dir = "."
 	if gen_out_dir == '':    
 		gen_out_dir = os.path.join(repo_dir, "build", "hpwh_data_model")
+	
+	max_wait_time_s = 10.
+	start_time_s = time.time()
+	while True:
+		if os.path.exists(data_model_dir):
+			time.sleep(1)				
+			lat = Lattice(data_model_dir, working_dir, gen_out_dir, False)
+			lat.generate_cpp_project()
+			time.sleep(1)
+			return True
 		
-	lat = Lattice(data_model_dir, working_dir, gen_out_dir, False)
-	lat.generate_cpp_project()
+		if time.time() - start_time_s > max_wait_time_s:
+			print("hpwh_data_model code generation failed.")
+			return False
 
 # main
 if __name__ == "__main__":
-
 		repo_dir = "../../../"
 		data_model_dir = ""
 		gen_out_dir = ""
