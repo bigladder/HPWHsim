0a9b7481dabd8a0b982fd0cc358cd9745119e84f
spahrenk <spahrenk@gmail.com>
2025-04-20 17:49:19 -0600

    Fix spacing.

--- src/HPWHversion.in.hh
+++ src/HPWHversion.in.hh
@@ -6,9 +6,9 @@
 #define HPWHversion_hh
 
 // clang-format off
-#define HPWHVRSN_MAJOR @ HPWHsim_VRSN_MAJOR@
-#define HPWHVRSN_MINOR @ HPWHsim_VRSN_MINOR@
-#define HPWHVRSN_PATCH @ HPWHsim_VRSN_PATCH@
+#define HPWHVRSN_MAJOR @HPWHsim_VRSN_MAJOR@
+#define HPWHVRSN_MINOR @HPWHsim_VRSN_MINOR@
+#define HPWHVRSN_PATCH @HPWHsim_VRSN_PATCH@
 #define HPWHVRSN_META "@HPWHsim_VRSN_META@"
 // clang-format on
 
