ba34f5fd71f9874fd665bae48e59e531394249f8
Phil Ahrenkiel <spahrenk@gmail.com>
2025-04-21 13:34:37 -0600

    Update lattice, models.

--- vendor/hpwh_data_model/cpp/include/hpwh_data_model/RSRESISTANCEWATERHEATSOURCE.h
+++ vendor/hpwh_data_model/cpp/include/hpwh_data_model/RSRESISTANCEWATERHEATSOURCE.h
@@ -21,43 +21,43 @@ namespace hpwh_data_model {
 		};
 		struct ProductInformation {
 			std::string manufacturer;
-			bool manufacturer_is_set;
+			bool manufacturer_is_set = false;
 			const static std::string_view manufacturer_units;
 			const static std::string_view manufacturer_description;
 			const static std::string_view manufacturer_name;
 			std::string model_number;
-			bool model_number_is_set;
+			bool model_number_is_set = false;
 			const static std::string_view model_number_units;
 			const static std::string_view model_number_description;
 			const static std::string_view model_number_name;
 		};
 		struct Description {
 			rsresistancewaterheatsource::ProductInformation product_information;
-			bool product_information_is_set;
+			bool product_information_is_set = false;
 			const static std::string_view product_information_units;
 			const static std::string_view product_information_description;
 			const static std::string_view product_information_name;
 		};
 		struct Performance {
 			double input_power;
-			bool input_power_is_set;
+			bool input_power_is_set = false;
 			const static std::string_view input_power_units;
 			const static std::string_view input_power_description;
 			const static std::string_view input_power_name;
 		};
 		struct RSRESISTANCEWATERHEATSOURCE : ashrae205::HeatSourceTemplate {
 			core::Metadata metadata;
-			bool metadata_is_set;
+			bool metadata_is_set = false;
 			const static std::string_view metadata_units;
 			const static std::string_view metadata_description;
 			const static std::string_view metadata_name;
 			rsresistancewaterheatsource::Description description;
-			bool description_is_set;
+			bool description_is_set = false;
 			const static std::string_view description_units;
 			const static std::string_view description_description;
 			const static std::string_view description_name;
 			rsresistancewaterheatsource::Performance performance;
-			bool performance_is_set;
+			bool performance_is_set = false;
 			const static std::string_view performance_units;
 			const static std::string_view performance_description;
 			const static std::string_view performance_name;
