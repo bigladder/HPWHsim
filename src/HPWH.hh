dddd099aaecfe370ec6a9f16f8ca10fc87617432
Phil Ahrenkiel <spahrenk@gmail.com>
2025-04-24 13:23:39 -0600

    Overload getHeatSource fnc.

--- src/HPWH.hh
+++ src/HPWH.hh
@@ -1116,6 +1116,9 @@ class HPWH : public Courier::Sender
     /**< returns the enum value for what type of heat source the Nth heat source is  */
 
     /// get a pointer to the Nth heat source
+    HeatSource* getNthHeatSource(int N);
+
+    /// get a pointer to the Nth heat source
     bool getNthHeatSource(int N, HPWH::HeatSource*& heatSource);
 
     double getExternalVolumeHeated(UNITS units = UNITS_L) const;
