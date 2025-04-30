abf4b548d51db525cd188c8551b7406846f4d985
Phil Ahrenkiel <spahrenk@gmail.com>
2025-04-30 15:32:45 -0600

    Write json models to headers.

--- scripts/python/data_model/incorp_models.py
+++ scripts/python/data_model/incorp_models.py
@@ -1,6 +1,6 @@
-# 'poetry run python incorp_models.py IHPWH_models.json ../../../build ../../../src/models'
+# 'poetry run python incorp_models.py IHPWH_models.json ../../../build'
 # or
-# 'poetry run python incorp_models.py CWHS_models.json ../../../build ../../../src/models'
+# 'poetry run python incorp_models.py CWHS_models.json ../../../build'
 # calls `hpwh convert' for each model in models_list_file json,
 # then creates a readable C++ file containing that data.
 
@@ -10,7 +10,7 @@ import sys
 import json
 import subprocess
 
-def incorp_models(models_list_file, build_dir, output_dir):
+def incorp_models(models_list_file, build_dir):
     
 	orig_dir = str(Path.cwd())
 	os.chdir(build_dir)
@@ -22,41 +22,47 @@ def incorp_models(models_list_file, build_dir, output_dir):
 		json_file.close()
 
 		app_cmd = os.path.join(abs_build_dir , 'src', 'hpwh', 'hpwh')
-		if output_dir == "":
-			output_dir = os.path.join(abs_build_dir , "test", "output")
+		output_dir = os.path.join(abs_build_dir , "test", "output")
 
 		if not os.path.exists(output_dir):
 			os.mkdir(output_dir)
 			
 		for model in json_data:  
 			convert_list = [app_cmd, 'convert', '-m', str(model["number"]), '-n', '-d', output_dir, '-f', model["name"]]
-		  
+			#print(convert_list)	  
+			result = subprocess.run(convert_list, stdout=subprocess.PIPE, text=True)	
+				
 			model_json_path = os.path.join(output_dir, model["name"] + ".json")
 		  
+			data = {}
 			try:
 				with open(model_json_path) as json_data:
-						data = json.load(json_data)
-						model_text_path = os.path.join("../../../src/models", model["name"] + ".h"), 
+					data = json.load(json_data)
+					json_data.close()
 			except:
-				continue
+				continue	
+			model_text_path = os.path.join("../../../src/models", model["name"] + ".h")
+
+			model_text = "constexpr char *j_" + model["name"] + "= R\"config("
+			model_text = model_text + json.dumps(data)
+			model_text = model_text + ")config\";"
 			
+			try:	
+				with open(model_text_path, "w") as model_text_file:
+					model_text_file.write(model_text)
+					model_text_file.close()
+			except:
+				print("Failed to create file")
 
-
-		  print(convert_list)
-		  result = subprocess.run(convert_list, stdout=subprocess.PIPE, text=True)
-
-
-
-  os.chdir(orig_dir)
+	os.chdir(orig_dir)
   
 # main
 if __name__ == "__main__":
     n_args = len(sys.argv) - 1
 
-    if n_args == 3:
+    if n_args == 2:
         models_list_file = sys.argv[1]
         build_dir = sys.argv[2]
-        output_dir = sys.argv[3]
 
-        incorp_models(models_list_file, build_dir, output_dir)
+        incorp_models(models_list_file, build_dir)
 
