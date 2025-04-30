35483626e2e4176885f90285524376481549e66f
Phil Ahrenkiel <spahrenk@gmail.com>
2025-04-30 14:06:51 -0600

    Set is_sets.

--- src/Condenser.cc
+++ src/Condenser.cc
@@ -628,6 +628,8 @@ void HPWH::Condenser::to(
         checkTo(
             tempGridValues[1], lookup_vars.heating_capacity_is_set, lookup_vars.heating_capacity);
 
+        map.grid_variables_is_set = true;
+        map.lookup_variables_is_set = true;
         perf.performance_map_is_set = true;
     }
     hs.performance_is_set = true;
@@ -735,6 +737,8 @@ void HPWH::Condenser::to(hpwh_data_model::rsairtowaterheatpump::RSAIRTOWATERHEAT
         checkTo(
             heatingCapacities_W, lookup_vars.heating_capacity_is_set, lookup_vars.heating_capacity);
 
+        map.grid_variables_is_set = true;
+        map.lookup_variables_is_set = true;
         perf.performance_map_is_set = true;
     }
     else // convert to grid
@@ -772,6 +776,8 @@ void HPWH::Condenser::to(hpwh_data_model::rsairtowaterheatpump::RSAIRTOWATERHEAT
         checkTo(
             tempGridValues[1], lookup_vars.heating_capacity_is_set, lookup_vars.heating_capacity);
 
+        map.grid_variables_is_set = true;
+        map.lookup_variables_is_set = true;
         perf.performance_map_is_set = true;
     }
     hs.performance_is_set = true;
