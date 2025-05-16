2129729a61245c853418b8a79aff51395e2351f3
Phil Ahrenkiel <spahrenk@gmail.com>
2025-05-16 16:46:42 -0600

    Set interp for Sanco2.

--- src/Condenser.cc
+++ src/Condenser.cc
@@ -225,14 +225,14 @@ void HPWH::Condenser::makeBtwxt()
     auto is_Mitsubishi = (hpwh->model == MODELS_MITSUBISHI_QAHV_N136TAU_HPB_SP);
     auto is_NyleMP =
         ((MODELS_NyleC60A_MP <= hpwh->model) && (hpwh->model <= MODELS_NyleC250A_C_MP));
-    auto is_Sanden =
+    auto is_Sanco2 =
         ((hpwh->model == MODELS_SANCO2_83) || (hpwh->model == MODELS_SANCO2_GS3_45HPA_US_SP) ||
          (hpwh->model == MODELS_SANCO2_119) || (hpwh->model == MODELS_SANCO2_43));
 
     std::vector<Btwxt::GridAxis> grid_axes = {};
     std::size_t iAxis = 0;
     {
-        auto interpMethod = (is_Mitsubishi || is_NyleMP || is_Sanden || is_integrated)
+        auto interpMethod = (is_Mitsubishi || is_NyleMP || is_Sanco2 || is_integrated)
                                 ? Btwxt::InterpolationMethod::linear
                                 : Btwxt::InterpolationMethod::cubic;
         auto extrapMethod = (is_Mitsubishi || is_NyleMP) ? Btwxt::ExtrapolationMethod::constant
