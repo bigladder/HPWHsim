e9e002377b266e65bdcce3ab1f3adaa579705460
Phil Ahrenkiel <spahrenk@gmail.com>
2025-06-19 12:00:00 -0600

    Remove debug break.

--- src/hpwh/run.cpp
+++ src/hpwh/run.cpp
@@ -395,10 +395,6 @@ void run(const std::string specType,
             }
         }
 
-        if (i == 228)
-        {
-            std::cout << "\n";
-        }
         // Run the step
         hpwh.runOneStep(allSchedules[0][i],           // Inlet water temperature (C)
                         GAL_TO_L(allSchedules[1][i]), // Flow in gallons
