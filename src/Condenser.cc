6bc40668d7ce9c324d55d2b480f08363f8b4f7b8
Phil Ahrenkiel <spahrenk@gmail.com>
2025-06-30 08:11:45 -0600

    Add 20.C.

--- src/Condenser.cc
+++ src/Condenser.cc
@@ -684,7 +684,7 @@ void HPWH::Condenser::to(hpwh_data_model::rsairtowaterheatpump::RSAIRTOWATERHEAT
             std::vector<double> envTs_C = {
                 minT, 0.,   0.5, 1.,   1.5,    2.,   2.5, 3.,   3.5, 4.,   4.5,        5.,
                 5.5,  6.,   6.5, 7.,   7.2223, 7.5,  8.,  8.5,  9.,  9.5,  10.,        10.5,
-                11.,  11.5, 12., 12.5, 13.,    13.5, 14., 14.5, 15., 15.5, 15.5555556, maxT};
+                11.,  11.5, 12., 12.5, 13.,    13.5, 14., 14.5, 15., 15.5, 15.5555556, 20., maxT};
 
             trimGridVector(envTs_C, minT, maxT);
             for (auto& envT_C : envTs_C)
