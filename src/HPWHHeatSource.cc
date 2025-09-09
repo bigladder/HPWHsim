9a1030eeed207dc3b1cdad6f20b3e15c4f455672
Phil Ahrenkiel <spahrenk@gmail.com>
2025-09-09 11:21:51 -0600

    Update 8 refs using smooth step-function.

--- src/HPWHHeatSource.cc
+++ src/HPWHHeatSource.cc
@@ -360,7 +360,7 @@ double HPWH::HeatSource::heat(double cap_kJ, const double maxSetpointT_C)
         constexpr double withhold_pow = 2.;
         double nodeCap_kJ = heatDistribution[i] * cap_kJ;
         double frac = heatDistribution[i] / maxWeight;
-        double withheldCap_kJ = /*(frac > 0.? 0.: 1.)*/std::pow(1. - frac, withhold_pow) * leftoverCap_kJ;
+        double withheldCap_kJ = std::pow(1. - frac, withhold_pow) * leftoverCap_kJ; // smooth step-function
         double carriedCap_kJ = leftoverCap_kJ - withheldCap_kJ;
         double availableCap_kJ = nodeCap_kJ + carriedCap_kJ;
         if (availableCap_kJ > 0.)
